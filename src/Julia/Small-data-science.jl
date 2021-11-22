563f016b0e6a772a7bebccc7e8e861d107a862fa
Minsoo <mmkim1210@gmail.com>
2021-11-22 15:43:30 -0800

    add plotting code

--- src/Julia/Small-data-science.jl
+++ src/Julia/Small-data-science.jl
@@ -2,9 +2,11 @@
 using CSV, DataFrames, CodecZlib, Mmap, DataFramesMeta, Chain, Plots
 using Statistics: mean
 using FloatingTableView: browse
+using GLM, StatsModels
 
 # Download GENCODE v36
-run(`curl ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_36/gencode.v36.annotation.gtf.gz --output gencode.v36.annotation.gtf.gz`)
+download("ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_36/gencode.v36.annotation.gtf.gz", "gencode.v36.annotation.gtf.gz")
+#run(`curl ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_36/gencode.v36.annotation.gtf.gz --output gencode.v36.annotation.gtf.gz`)
 
 # Load GENCODE v36
 @time gencode = CSV.File(transcode(GzipDecompressor, 
@@ -17,6 +19,8 @@ run(`curl ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_36/gen
 size(gencode)
 names(gencode)
 typeof(gencode)
+first(gencode, 5)
+last(gencode, 5)
 gencode[:, ["start", "end"]]
 gencode[:, r".e"]
 
@@ -27,7 +31,7 @@ end
 #gencode_tx = gencode[gencode.feature .== "transcript", :]
 #gencode_tx = filter(row -> row.feature == "transcript" && row.seqnames == "chr1", gencode)
 
-# Extract transcript id's and transcript support level (tsl)
+# Extract transcript id's and transcript support lev  el (tsl)
 for meta in ["transcript_id", "transcript_support_level"]
   storage = Vector(undef, size(gencode)[1])
   for (n, m) in enumerate(match.(Regex("$(meta) \"(.*?)\";"), gencode.info))
@@ -76,11 +80,47 @@ end
 end
 
 # Number of genes per chromosome
-@chain gencode begin
+df = @chain gencode begin
   @subset(:feature .== "gene")
   groupby(:seqnames)
   @combine(:count = length(:score))
+  @subset(:seqnames .!= "chrX", :seqnames .!= "chrY", :seqnames .!= "chrM")
 end
+
+df.seqnames = parse.(Int64, replace.(df.seqnames, "chr" => ""))
+lm = fit(LinearModel, @formula(count ~ seqnames), df)
+summary(lm)
+coef(lm)
+f(x) = coef(lm)[2] * x + coef(lm)[1]
+plot(df.seqnames, df.count, 
+    smooth = true, 
+    seriestype = :scatter, 
+    title = "chromosome # vs # of genes", 
+    linewidth = 8,
+    linealpha = 0.5,
+    line = (:black, 0.5, 6, :solid),
+    size = (600, 300),
+    label = nothing,
+    xticks = (1:23),
+    xlabel = "chromosome #",
+    ylabel = "# of genes",
+    background_color = :white,
+    framestyle = :box, 
+    widen = false,
+    ylims = (0, 6000),
+    grid = (0.3, :dot))
+plot!(f, 0, 23, label = "linear model")
 
 varinfo()
 versioninfo()
+
+df = DataFrame(transcript_id = unique(gencode.transcript_id)[2:4], test = 2)
+@chain gencode begin
+  @subset(.!ismissing.(:transcript_id))
+  leftjoin(df, _; on = :transcript_id)
+end
+
+describe(gencode[:, ["start", "end"]], :mean, :std, :min, :q25, :median, 
+  :q75, :max, :eltype, :nunique, :first, :last, :nmissing)
+
+filter(endswith("id"), names(gencode))
